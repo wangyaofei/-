3a1ce47641bdbd7d6e87d8d49f42b87b5aaf13b9
wangyaofei <wangyaofei@imufe.edu.cn>
2018-10-17 23:10:12 +0800

    JDBC1

---
+++ src/com/cppmanage/util/JDBCUtil.java
@@ -0,0 +1,34 @@
+package com.cppmanage.util;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.util.Properties;
+import javax.sql.DataSource;
+
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+
+public class JDBCUtil {
+	public static DataSource ds = null;
+	static {
+		try {
+			Properties p = new Properties();
+			FileInputStream in = new FileInputStream("resource/db.properties");
+			p.load(in);
+			ds = DruidDataSourceFactory.createDataSource(p);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Connection getConn() {
+		try {
+			return ds.getConnection();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+}
---
+++ src/dbcreate.sql
@@ -0,0 +1,11 @@
+create database cppmanage;
+
+use cppmanage;
+
+create TABLE student
+(
+    id VARCHAR(16) PRIMARY KEY,
+    name VARCHAR(32),
+    password varchar(32),
+    tel VARCHAR(16)
+);
