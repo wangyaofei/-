7c5c5065e8ea7099e495e7b00bb5a7559796a267
wang <wang@WIN-VDM92EFC39A.localdomain>
2019-08-30 16:15:05 +0800

    DAOOK

--- src/com/cppmanage/daoimpl/MProjectDAOImpl.java
+++ src/com/cppmanage/daoimpl/MProjectDAOImpl.java
@@ -4,6 +4,8 @@ import java.sql.SQLException;
 import java.util.List;
 
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
 
 import com.cppmanage.dao.IMProjectDAO;
 import com.cppmanage.domain.MProject;
@@ -16,31 +18,37 @@ public class MProjectDAOImpl implements IMProjectDAO {
 	@Override
 	public void updateproject(MProject project) throws SQLException {
 		// TODO Auto-generated method stub
-
+		String sql = "update mproject set proname=?,proinfo=?,prodegree=? where proid=?";
+		qr.update(sql,project.getProname(),project.getProinfo(),project.getProdegree(),project.getProid());
 	}
 
 	@Override
 	public void deleteproject(String id) throws SQLException {
 		// TODO Auto-generated method stub
-
+		String sql = "delete from mproject where proid=?";
+		qr.update(sql,id);
 	}
 
 	@Override
 	public List<MProject> getAll() throws SQLException {
 		// TODO Auto-generated method stub
-		return null;
+		String sql = "select * from mproject";
+		return qr.query(sql, new BeanListHandler<MProject>(MProject.class));
 	}
 
 	@Override
 	public int addProject(MProject project) throws SQLException {
 		// TODO Auto-generated method stub
+		String sql = "insert into mproject values(?,?,?,?)";
+		qr.update(sql,project.getProid(),project.getProname(),project.getProinfo(),project.getProdegree());
 		return 0;
 	}
 
 	@Override
 	public MProject getProjectWithID(String id) throws SQLException {
 		// TODO Auto-generated method stub
-		return null;
+		String sql = "select * from mproject where proid=?";
+		return qr.query(sql, new BeanHandler<MProject>(MProject.class),id);
 	}
 
 }
--- src/com/cppmanage/daoimpl/ScoreDAOImpl.java
+++ src/com/cppmanage/daoimpl/ScoreDAOImpl.java
@@ -4,6 +4,8 @@ import java.sql.SQLException;
 import java.util.List;
 
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
 
 import com.cppmanage.dao.IScoreDAO;
 import com.cppmanage.domain.Score;
@@ -17,31 +19,37 @@ public class ScoreDAOImpl implements IScoreDAO {
 	@Override
 	public void updatescore(Score score) throws SQLException {
 		// TODO Auto-generated method stub
-
+		String sql = "update score set scrvalue=?,tchid=?,stuid=? where scrid=?";
+		qr.update(sql,score.getScrvalue(),score.getTchid(),score.getStuid(),score.getScrid());
 	}
 
 	@Override
 	public void deletescore(String id) throws SQLException {
 		// TODO Auto-generated method stub
-
+		String sql = "delete from score where scrid=?";
+		qr.update(sql,id);
 	}
 
 	@Override
 	public List<Score> getAll() throws SQLException {
 		// TODO Auto-generated method stub
-		return null;
+		String sql = "select * from score";
+		return qr.query(sql, new BeanListHandler<Score>(Score.class));
 	}
 
 	@Override
 	public int addScore(Score score) throws SQLException {
 		// TODO Auto-generated method stub
+		String sql = "insert into score values(?,?,?,?)";
+		qr.update(sql,score.getScrid(),score.getScrvalue(),score.getTchid(),score.getStuid());
 		return 0;
 	}
 
 	@Override
 	public Score getScoreWithID(String id) throws SQLException {
 		// TODO Auto-generated method stub
-		return null;
+		String sql = "select * from score where scrid=?";
+		return qr.query(sql, new BeanHandler<Score>(Score.class),id);
 	}
 
 }
